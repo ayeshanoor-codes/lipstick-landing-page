262eb48d4b3bdc8b13d86157d876981268eeb70f
Ayesha <noor.developer.13@gmail.com>
2025-11-28 00:25:35 +0500

    commit all

---
+++ app.js
@@ -0,0 +1,18 @@
+ let midnightBtn = document.getElementById("midnight");
+      let starlightBtn = document.getElementById("starlight");
+      let spacegrayBtn = document.getElementById("spacegray");
+      let silverBtn = document.getElementById("silver");
+      let apple = document.getElementById("apple");
+
+      midnightBtn.onclick = function () {
+        apple.style.backgroundImage = "url(Assets/Velvet_Reign_Queen_Luscious_Cosmetics_09b9d321-ac80-44c2-a49f-ed76c1e63f9b.webp)";
+      };
+      starlightBtn.onclick = function () {
+        apple.style.backgroundImage = "url(Assets/09_Royalty_8cce1399-b39f-4e5c-a722-9910071105b1.webp)";
+      };
+      spacegrayBtn.onclick = function () {
+        apple.style.backgroundImage = "url(Assets/02_Princess_8c0dc17a-afbd-432c-b354-dceb967e7b2a.webp)";
+      };
+      silverBtn.onclick = function () {
+        apple.style.backgroundImage = "url(Assets/Tiara.webp)";
+      };
